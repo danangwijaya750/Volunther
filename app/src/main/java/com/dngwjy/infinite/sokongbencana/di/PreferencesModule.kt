338dcb311f18e6a06739c2b7328618da89ff1e31
Danang <danangwijaya750@gamail.com>
2019-03-02 22:41:03 +0700

    remove PrefModule

--- app/src/main/java/com/dngwjy/infinite/sokongbencana/di/PreferencesModule.kt
+++
@@ -1,8 +0,0 @@
-package com.dngwjy.infinite.sokongbencana.di
-
-import com.dngwjy.infinite.sokongbencana.data.shared.Preferences
-import org.koin.dsl.module.module
-
-val preferencesModule=module{
-    Preferences(get())
-}
