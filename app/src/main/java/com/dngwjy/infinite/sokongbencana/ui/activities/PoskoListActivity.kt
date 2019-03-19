1216f665bfd8976e039d2001642843eb71864b38
Danang <danangwijaya750@gamail.com>
2019-03-19 21:04:14 +0700

    add 1 activity

---
+++ app/src/main/java/com/dngwjy/infinite/sokongbencana/ui/activities/PoskoListActivity.kt
@@ -0,0 +1,12 @@
+package com.dngwjy.infinite.sokongbencana.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PoskoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posko_list)
+    }
+}
